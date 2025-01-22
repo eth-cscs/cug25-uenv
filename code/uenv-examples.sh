9c21edd9fcfd8e1717be3a2081b1c7441493b05b
bcumming <bcumming@cscs.ch>
2025-01-22 10:24:34 +0100

    final polish for abstract submission

--- code/uenv-examples.sh
+++ code/uenv-examples.sh
@@ -4,3 +4,5 @@ uenv image find prgenv-gnu
 uenv pull prgenv-gnu/24.11
 # start a shell with modules enabled
 uenv start prgenv-gnu/24.11 --view=modules
+# run the latest nvim in its own environment
+uenv run editors/25.1 --view=ed -- nvim
